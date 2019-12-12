d3c21cec7eef98d0a87be9d7de6131f884bf09b2
ChidiChuks <chidiebere_chukwuma@yahoo.com>
2019-12-12 18:23:21 +0100

    Handling the async storage of the submit button in register screen

--- screens/Register.js
+++ screens/Register.js
@@ -3,6 +3,8 @@ import { StyleSheet, ImageBackground, Text, View, TextInput, Image, Button, } fr
 
 import Card from '../components/Card';
 
+const userInfo = {firstName: 'Akanda', lastName: 'Ara', phone: '08136266387', email: 'akadanzara@gmail.com', password: 'damond'}
+
 export default class Register extends Component {
   static navigationOptions = {
     headerStyle: {
@@ -39,9 +41,9 @@ export default class Register extends Component {
           <Card style={styles.inputContainer}>
             <TextInput style={{ width: '90%', height: 40, borderColor: 'gray', borderWidth: 1, borderTopWidth: 0, borderLeftWidth: 0, borderRightWidth: 0, alignItems: "center", padding: 5, margin: 5 }} placeholder="First Name" onChangeText={(firstName)=>this.setState({firstName})} value={this.state.firstName} />
             <TextInput style={{ width: '90%', height: 40, borderColor: 'gray', borderWidth: 1, borderTopWidth: 0, borderLeftWidth: 0, borderRightWidth: 0, alignItems: "center", padding: 5, margin: 5 }} placeholder="Last Name" onChangeText={(lastName)=>this.setState({lastName})} value={this.state.lastName} />
-            <TextInput style={{ width: '90%', height: 40, borderColor: 'gray', borderWidth: 1, borderTopWidth: 0, borderLeftWidth: 0, borderRightWidth: 0, alignItems: "center", padding: 5, margin: 5 }} placeholder="Phone number" keyboardType="number-pad" onChangeText={(phone)=>this.setState{phone}} value={this.state.phone} />
-            <TextInput style={{ width: '90%', height: 40, borderColor: 'gray', borderWidth: 1, borderTopWidth: 0, borderLeftWidth: 0, borderRightWidth: 0, alignItems: "center", padding: 5, margin: 5 }} placeholder="Email" onChangeText={(email)=>this.setState{email}} value={this.state.email} />
-            <TextInput style={{ width: '90%', height: 40, borderColor: 'gray', borderWidth: 1, borderTopWidth: 0, borderLeftWidth: 0, borderRightWidth: 0, alignItems: "center", padding: 5, margin: 5 }} placeholder="Password" secureTextEntry={true} onChangeText={(password)=>this.setState{password}} value={this.state.password} />
+            <TextInput style={{ width: '90%', height: 40, borderColor: 'gray', borderWidth: 1, borderTopWidth: 0, borderLeftWidth: 0, borderRightWidth: 0, alignItems: "center", padding: 5, margin: 5 }} placeholder="Phone number" keyboardType="number-pad" onChangeText={(phone)=>this.setState({phone})} value={this.state.phone} />
+            <TextInput style={{ width: '90%', height: 40, borderColor: 'gray', borderWidth: 1, borderTopWidth: 0, borderLeftWidth: 0, borderRightWidth: 0, alignItems: "center", padding: 5, margin: 5 }} placeholder="Email" onChangeText={(email)=>this.setState({email})} value={this.state.email} />
+            <TextInput style={{ width: '90%', height: 40, borderColor: 'gray', borderWidth: 1, borderTopWidth: 0, borderLeftWidth: 0, borderRightWidth: 0, alignItems: "center", padding: 5, margin: 5 }} placeholder="Password" secureTextEntry={true} onChangeText={(password)=>this.setState({password})} value={this.state.password} />
           </Card>
             <Text style={styles.submit} onPress={this._register}>Submit</Text>
           <Text style={styles.login} onPress={() => navigate('Login')}>LogIn</Text>
@@ -50,10 +52,13 @@ export default class Register extends Component {
  )
  }
  _register = async() => {
-   if(this.state.firstName === '' && this.state.lastName === '' && this.state.phone === '' && this.state.email === '' && this.state.password === '') {
-     alert('Kindly complete all fields in the form');
-   } else {
+   if (this.state.firstName === '' && this.state.lastName === '' && this.state.phone === '' && this.state.email === '' && this.state.password === '') {
+     alert('Kindly fill all fields in the form');
+   }
+   else if(userInfo.firstName === this.state.firstName && userInfo.LastName === this.state.lastName && userInfo.phone === this.state.phone && userInfo.email === this.state.email && userInfo.password === this.state.password) {
     this.props.navigation.navigate('Browse');
+   } else {
+    alert('You should look into your data provided and correct or fill all fields ');
    }
  }
 };
